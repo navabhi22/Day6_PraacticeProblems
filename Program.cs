d53fb4c6a183fe85e86d8426e74bb8a79875d8af
navabhi22 <navabhi22@gmail.com>
2022-02-22 22:54:46 +0530

    Print Day5 Prcatice Problems

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 6 Practice Problems ");
+            FlipCoin.FlipTheCoin();
+        }
+    }
+}
